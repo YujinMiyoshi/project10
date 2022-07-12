df3d4b0d8358034e15097a3b0b5152c9c6b8bb93
eugene <eugene.earth.solar@gmail.com>
2022-07-13 04:08:19 +0900

    commit 4

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -3,6 +3,6 @@ from .views import nippoDetailView, nippoListView, nippoCreateView
 
 urlpatterns = [
     path("", nippoListView, name='nippo_list'),
-    path("detail/<int:number>/", nippoDetailView, name='detail'),
+    path("detail/<int:pk>/", nippoDetailView, name='detail'),
     path("create/", nippoCreateView, name='create'),
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from random import randint
+from .models import NippoModel
 
 def nippoListView(request):
-    return render(request, "nippo/nippo_list.html")
+    ctx={}
+    qs = NippoModel.objects.all()
+    ctx["object_list"] = qs
+    return render(request, "nippo/nippo_list.html", ctx)
 
-def nippoDetailView(request,number):
+def nippoDetailView(request, pk):
     template_name = "nippo/nippo_detail.html"
     random_int = randint(1,10)
+    q = NippoModel.objects.get(pk = pk)
     ctx = {
         "random_number": random_int,
-        "number": number,
+        "number": pk,
+        "object":q,
     }
     return render(request, template_name, ctx)
 
@@ -19,6 +25,8 @@ def nippoCreateView(request):
     if request.POST:
         title = request.POST["title"]
         content = request.POST["content"]
+        obj = NippoModel(title=title, content=content)
+        obj.save()
 
     return render(request, template_name)
 # Create your views here.
